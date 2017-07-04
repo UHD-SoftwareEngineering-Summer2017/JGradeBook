9737029f454453dbcf47723da5ac49a5d1d9047f
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:28:15 -0500

    Update AdminAddRunner.java

--- JGradeBook/src/windowRunners/AdminAddRunner.java
+++ JGradeBook/src/windowRunners/AdminAddRunner.java
@@ -1,3 +1,7 @@
+/**
+ AdminAddRunner controls the window for adding new students.
+ */
+
 package windowRunners;
 
 import database.FileInputOutput;
@@ -7,27 +11,40 @@ import javax.swing.JOptionPane;
 
 public class AdminAddRunner
 {
-    private AdminAddWindow aaw;
-    
+    private AdminAddWindow aaw; // Associated AdminAddWindow
+
+    /**
+     Constructor.
+     */
     public AdminAddRunner()
     {
         aaw = new AdminAddWindow(this);
         aaw.setVisible(true);
     }
+
+    /**
+     clickedAdd creates a new student from the entered information.
+     */
     public void clickedAdd(int studentID, String password, String name)
     {
         try
         {
+            // Save the student in a new file and return to the students window.
             Student student = new Student(studentID, password, name);
             FileInputOutput.createNewStudentFile(student);
             aaw.dispose();
             AdminRunner ar = new AdminRunner();
         }
+        // Do not proceed if the entered student ID already exists.
         catch(Exception e)
         {
             JOptionPane.showMessageDialog(null, "ID " + studentID + " already exists.", "New Student", JOptionPane.WARNING_MESSAGE);
         }
     }
+
+    /**
+     clickedCancel returns to the students window.
+     */
     public void clickedCancel()
     {
         aaw.dispose();
