b4a84926b324f38fd3d53d76f7641474d2aeadde
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:29:34 -0500

    Update AdminRunner.java

--- JGradeBook/src/windowRunners/AdminRunner.java
+++ JGradeBook/src/windowRunners/AdminRunner.java
@@ -1,3 +1,6 @@
+/**
+ AdminRunner controls the administrator window that views all the students.
+ */
 package windowRunners;
 
 import database.Student;
@@ -6,25 +9,40 @@ import windows.AdminWindow;
 
 public class AdminRunner
 {
-    private AdminWindow aw;
-    private Student[] students;
-    
+    private AdminWindow aw;     // Associated AdminWindow
+    private Student[] students; // List of all the students
+
+    /**
+     Constructor.
+     */
     public AdminRunner()
     {
         students = FileInputOutput.getStudentsFromFolder();
         aw = new AdminWindow(this, students);
         aw.setVisible(true);
     }
+
+    /**
+     clickedAdd opens the window for adding a new student.
+     */
     public void clickedAdd()
     {
         aw.dispose();
         AdminAddRunner aar = new AdminAddRunner();
     }
+
+    /**
+     clickedDone logs out the administrator and returns to the login window.
+     */
     public void clickedDone()
     {
         aw.dispose();
         LoginRunner lr = new LoginRunner();
     }
+
+    /**
+     clickedStudent opens the administrator's student window.
+     */
     public void clickedStudent(int studentIndex)
     {
         aw.dispose();
