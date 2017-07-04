d9179086e3420ad38091624843e449da22628f59
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:29:12 -0500

    Update AdminEditRunner.java

--- JGradeBook/src/windowRunners/AdminEditRunner.java
+++ JGradeBook/src/windowRunners/AdminEditRunner.java
@@ -1,3 +1,6 @@
+/**
+ AdminEditRunner controls the student editing window.
+ */
 package windowRunners;
 
 import database.FileInputOutput;
@@ -9,25 +12,34 @@ import javax.swing.JOptionPane;
 
 public class AdminEditRunner
 {
-    private AdminEditWindow aew;
-    private Student student;
-    
+    private AdminEditWindow aew; // Associated AdminEditWindow
+    private Student student; // Associated student
+
+    /**
+     Constructor.
+     */
     public AdminEditRunner(Student student)
     {
         this.student = student;
         aew = new AdminEditWindow(this, student);
         aew.setVisible(true);
     }
-    
+
+    /**
+     clickedSave updates the student with new information.
+     */
     public void clickedSave(String name, String password, String courseData)
     {
+        // Create a new Student and StringTokenizers to read courseData.
         Student updateStudent = new Student(student.getStudentID(), password, name);
         StringTokenizer outerST = new StringTokenizer(courseData, "\n");
         StringTokenizer innerST;
         String courseName;
-        
+
+        // Read an entire line of course data.
         try
         {
+            // Read specific information in the course data line.
             while(outerST.hasMoreTokens())
             {
                 innerST = new StringTokenizer(outerST.nextToken(), " ");
@@ -37,12 +49,13 @@ public class AdminEditRunner
                     updateStudent.addGrade(courseName, Double.parseDouble(innerST.nextToken()));
             }
         }
+        // End the process if courseData is in the wrong format.
         catch(Exception e)
         {
             JOptionPane.showMessageDialog(null, "Course data is in the wrong format.", "Edit Student", JOptionPane.WARNING_MESSAGE);
             return;
         }
-        
+        // Replace the current student with the new student and return to the students window.
         try
         {
             FileInputOutput.replaceStudentFile(updateStudent);
@@ -52,11 +65,17 @@ public class AdminEditRunner
         aew.dispose();
         AdminStudentRunner asr = new AdminStudentRunner(updateStudent);
     }
+
+    /**
+     clickedDelete deletes the student file.
+     */
     public void clickedDelete()
     {
+        // Display a window to confirm the delete.
         int doDelete = JOptionPane.showConfirmDialog(null, "Delete student " + student.getStudentID() + "?", "Delete Student", JOptionPane.WARNING_MESSAGE);
         if(doDelete == JOptionPane.YES_OPTION)
         {
+            // Delete the student and return to the students window.
             try
             {
                 FileInputOutput.deleteStudentFile(student);
@@ -66,6 +85,10 @@ public class AdminEditRunner
             AdminRunner ar = new AdminRunner();
         }
     }
+
+    /**
+     clickedCancel returns to the administrator's student window.
+     */
     public void clickedCancel()
     {
         aew.dispose();
