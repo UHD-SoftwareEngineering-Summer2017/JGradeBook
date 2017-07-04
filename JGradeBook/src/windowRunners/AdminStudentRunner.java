4cf0c736fb682be6718b8ee822744a0b0bb812ae
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:30:26 -0500

    Update AdminStudentRunner.java

--- JGradeBook/src/windowRunners/AdminStudentRunner.java
+++ JGradeBook/src/windowRunners/AdminStudentRunner.java
@@ -1,3 +1,6 @@
+/**
+ AdminStudentRunner controls the administrator's student viewing window.
+ */
 package windowRunners;
 
 import database.Student;
@@ -6,25 +9,39 @@ import javax.swing.JOptionPane;
 
 public class AdminStudentRunner
 {
-    private AdminStudentWindow asw;
-    private Student student;
-    
+    private AdminStudentWindow asw; // Associated AdminStudentWindow
+    private Student student;        // Associated student
+
+    /**
+     Constructor.
+     */
     public AdminStudentRunner(Student student)
     {
         this.student = student;
         asw = new AdminStudentWindow(this, student);
         asw.setVisible(true);
     }
-    
+
+    /**
+     clickedGPA displays the student's calculated GPA.
+     */
     public void clickedGPA()
     {
         JOptionPane.showMessageDialog(null, student.getName() + "'s GPA is " + student.calculateGPA(), "GPA", JOptionPane.INFORMATION_MESSAGE);
     }
+
+    /**
+     clickedEdit opens the student editing window.
+     */
     public void clickedEdit()
     {
         asw.dispose();
         AdminEditRunner aew = new AdminEditRunner(student);
     }
+
+    /**
+     clickedDone returns to the students window.
+     */
     public void clickedDone()
     {
         asw.dispose();
