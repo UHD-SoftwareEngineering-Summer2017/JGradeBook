bcd0ee0957eae1b3c620bbdfc491b9c512d0f951
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:35:45 -0500

    Update LoginWindow.java

--- JGradeBook/src/windows/LoginWindow.java
+++ JGradeBook/src/windows/LoginWindow.java
@@ -1,3 +1,7 @@
+/**
+ LoginWindow displays the window for logging in students and administrators.
+ */
+
 package windows;
 
 import windowRunners.LoginRunner;
@@ -10,29 +14,36 @@ import java.awt.event.ActionEvent;
 
 public class LoginWindow extends JFrame implements ActionListener
 {
-    public static final int WIDTH = 300;
-    public static final int HEIGHT = 150;
-    private LoginRunner lr;
-    
+    public static final int WIDTH = 300;  // Width
+    public static final int HEIGHT = 150; // Height
+    private LoginRunner lr; // Associated LoginRunner
+
+    // Labels and text fields for entering usernames and passwords.
     private JPanel loginPanel;
     private JLabel usernameLabel;
     private JTextField usernameBox;
     private JLabel passwordLabel;
     private JPasswordField passwordBox;
-    
+
+    // Buttons for actions.
     private JPanel buttonPanel;
     private JButton loginButton;
     private JButton cancelButton;
-    
+
+    /**
+     Constructor.
+     */
     public LoginWindow(LoginRunner lr)
     {
+        // Setup basic window information.
         super("Database Login");
         this.lr = lr;
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        
+
+        // Add labels and text fields for user input.
         loginPanel = new JPanel(new GridLayout(4, 1));
         usernameLabel = new JLabel("Username");
         usernameBox = new JTextField();
@@ -43,7 +54,8 @@ public class LoginWindow extends JFrame implements ActionListener
         loginPanel.add(passwordLabel);
         loginPanel.add(passwordBox);
         add(loginPanel, BorderLayout.CENTER);
-        
+
+        // Add buttons for actions.
         buttonPanel = new JPanel(new GridLayout(1, 2));
         loginButton = new JButton("Login");
         loginButton.addActionListener(this);
@@ -53,7 +65,10 @@ public class LoginWindow extends JFrame implements ActionListener
         buttonPanel.add(cancelButton);
         add(buttonPanel, BorderLayout.SOUTH);
     }
-    
+
+    /**
+     actionPerformed chooses a LoginRunner action depending on the button clicked.
+     */
     public void actionPerformed(ActionEvent e)
     {
         String action = e.getActionCommand();
