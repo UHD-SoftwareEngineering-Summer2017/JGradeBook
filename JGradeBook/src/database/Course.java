6c889bfe2c9f83032c221b26b56977033ab9a657
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:25:25 -0500

    Update Course.java

--- JGradeBook/src/database/Course.java
+++ JGradeBook/src/database/Course.java
@@ -1,17 +1,29 @@
+/**
+ The Course class holds information about a student's course.
+ */
+
+
 package database;
 
 import java.util.ArrayList;
 
 public class Course
 {
-    private String name;
-    private ArrayList<Double> grades;
-    
+    private String name; // Course name
+    private ArrayList<Double> grades; //List of grades
+
+    /**
+     Normal constructor.
+     */
     public Course(String name)
     {
         setName(name);
         grades = new ArrayList();
     }
+
+    /**
+     Copy Constructor.
+     */
     public Course(Course course)
     {
         setName(course.getName());
@@ -19,19 +31,34 @@ public class Course
         for (int index = 0; index < course.getNumGrades(); index++)
             grades.add(course.grades.get(index));
     }
+
+    /**
+     name Setter.
+     */
     public void setName(String name)
     {
         this.name = name;
     }
+
+    /**
+     name Getter.
+     */
     public String getName()
     {
         return name;
     }
-    
+
+    /**
+     addGrade adds a grade to the course.
+     */
     public void addGrade(double grade)
     {
         grades.add(grade);
     }
+
+    /**
+     deleteGrade deletes a grade from the course.
+     */
     public void deleteGrade(double grade)
     {
         for (int index = 0; index < grades.size(); index++)
@@ -45,10 +72,18 @@ public class Course
         
         throw new RuntimeException("Course already does not have grade.");
     }
+
+    /**
+     getNumGrades returns the number of grades.
+     */
     public int getNumGrades()
     {
         return grades.size();
     }
+
+    /**
+     getGrades returns an array of the grades.
+     */
     public double[] getGrades()
     {
         double[] gradeList = new double[grades.size()];
