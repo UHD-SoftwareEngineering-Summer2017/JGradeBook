32cbbe6d42c02cb6f4ff25b34a17b294cb4e7384
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:31:15 -0500

    Update StudentRunner.java

--- JGradeBook/src/windowRunners/StudentRunner.java
+++ JGradeBook/src/windowRunners/StudentRunner.java
@@ -1,3 +1,6 @@
+/**
+ StudentRunner controls the student viewing window.
+ */
 package windowRunners;
 
 import database.Student;
@@ -6,20 +9,31 @@ import javax.swing.JOptionPane;
 
 public class StudentRunner
 {
-    private StudentWindow sw;
-    private Student student;
-    
+    private StudentWindow sw; // Associated StudentWindow
+    private Student student; // Associated student
+
+    /**
+     Constructor.
+     */
     public StudentRunner(Student student)
     {
         this.student = student;
         sw = new StudentWindow(this, student);
         sw.setVisible(true);
     }
+
+    /**
+     clickedDone logs out the user and returns to the login window.
+     */
     public void clickedDone()
     {
         sw.dispose();
         LoginRunner lr = new LoginRunner();
     }
+
+    /**
+     clickedGPA displays the student's calculated GPA.
+     */
     public void clickedGPA()
     {
         JOptionPane.showMessageDialog(null, "Your GPA is " + student.calculateGPA(), "GPA", JOptionPane.INFORMATION_MESSAGE);
