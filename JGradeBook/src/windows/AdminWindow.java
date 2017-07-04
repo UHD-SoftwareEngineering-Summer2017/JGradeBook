c8407b41ae3e421a3e2c4886066e392c3a95fd47
IronBadger51 <scottblick@ymail.com>
2017-07-03 23:35:11 -0500

    Update AdminWindow.java

--- JGradeBook/src/windows/AdminWindow.java
+++ JGradeBook/src/windows/AdminWindow.java
@@ -1,3 +1,7 @@
+/**
+ AdminWindow displays the window for viewing all the students.
+ */
+
 package windows;
 
 import windowRunners.AdminRunner;
@@ -13,32 +17,42 @@ import java.awt.event.ActionEvent;
 
 public class AdminWindow extends JFrame implements ActionListener
 {
-    public static final int WIDTH = 300;
-    public static final int MINIMUM_HEIGHT = 70;
-    public static final int ADD_HEIGHT_FACTOR = 21;
-    private AdminRunner ar;
-    private Student[] students;
-    
+    public static final int WIDTH = 300;            // Width
+    public static final int MINIMUM_HEIGHT = 70;    // Minimum height
+    public static final int ADD_HEIGHT_FACTOR = 21; // Height to add per student
+    private AdminRunner ar;                         // Associated AdminRunner
+    private Student[] students;                     // List of students
+
+    // Labels and buttons for the students.
     private JLabel viewStudentLabel;
     private JPanel studentPanel;
     private JButton[][] studentButtons;
     private JPanel buttonPanel;
+
+    // Buttons for actions.
     private JButton addButton;
     private JButton doneButton;
-    
+
+    /**
+     Constructor.
+     */
     public AdminWindow(AdminRunner ar, Student[] students)
     {
+        // Setup basic window information.
         super("Administrator Directory");
         this.ar = ar;
         this.students = students;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-        
+
+        // Add the instruction label.
         viewStudentLabel = new JLabel("Click student to view details.");
         add(viewStudentLabel, BorderLayout.NORTH);
-        
+
+        // Setup the buttons for students.
         setupStudentData();
-        
+
+        // Add buttons for actions.
         buttonPanel = new JPanel(new GridLayout(1, 2));
         addButton = new JButton("Add Student");
         addButton.addActionListener(this);
@@ -48,30 +62,41 @@ public class AdminWindow extends JFrame implements ActionListener
         buttonPanel.add(doneButton);
         add(buttonPanel, BorderLayout.SOUTH);
     }
-    
+
+    /**
+     setupStudentData initializes and adds buttons that hold student data.
+     */
     private void setupStudentData()
     {
+        // Create an array to hold the buttons.
         studentPanel = new JPanel(new GridLayout(students.length, 2, -1, -1));
         studentButtons = new JButton[students.length][2];
-        
+
+        // Cycle through the list of students.
         for (int index = 0; index < students.length; index++)
         {
+            // Add the student ID.
             studentButtons[index][0] = new JButton(String.valueOf(students[index].getStudentID()));
             studentButtons[index][0].addActionListener(this);
             studentButtons[index][0].setActionCommand(String.valueOf(index));
             studentPanel.add(studentButtons[index][0]);
-            
+
+            // Add the student name.
             studentButtons[index][1] = new JButton(students[index].getName());
             studentButtons[index][1].addActionListener(this);
             studentButtons[index][1].setActionCommand(String.valueOf(index));
             studentPanel.add(studentButtons[index][1]);
         }
-        
+
+        // Add the panel and set the window size.
         add(studentPanel, BorderLayout.CENTER);
         setSize(WIDTH, MINIMUM_HEIGHT + students.length*ADD_HEIGHT_FACTOR);
         setLocationRelativeTo(null);
     }
-    
+
+    /**
+     actionPerformed chooses an AdminRunner action depending on the button clicked.
+     */
     public void actionPerformed(ActionEvent e)
     {
         String action = e.getActionCommand();
